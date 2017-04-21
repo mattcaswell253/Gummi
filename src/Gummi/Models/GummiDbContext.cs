36c65985b0e8007775a2541a5ab7f013b879c45b
mattcaswell <matthewcaswell253@Gmail.com>
2017-04-21 08:48:18 -0700

    set up context file

---
+++ src/Gummi/Models/GummiDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gummi.Models
+{
+    public class GummiDbContext : DbContext
+    {
+        public GummiDbContext()
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Gummi;integrated security=True");
+        }
+        public GummiDbContext(DbContextOptions<GummiDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
--- src/Gummi/Models/Product.cs
+++ src/Gummi/Models/Product.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Gummi.Models
 {
+    [Table("Products")]
     public class Product
     {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Cost { get; set; }
+        public string Origin { get; set; }
+        
     }
 }
