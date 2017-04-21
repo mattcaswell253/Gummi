2337090e2caf02bf224697063376c6b3cd2928b8
mattcaswell <matthewcaswell253@Gmail.com>
2017-04-21 10:48:27 -0700

    set up crud functionality

--- src/Gummi/Controllers/ProductsController.cs
+++ src/Gummi/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Gummi.Models;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,6 +18,49 @@ namespace Gummi.Controllers
         {
             return View(db.Products.ToList());
         }
+
+        public IActionResult Details(int id)
+        {
+            var thisItem = db.Products.FirstOrDefault(products => products.ProductId == id);
+            return View(thisItem);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var thisItem = db.Products.FirstOrDefault(products => products.ProductId == id);
+            return View(thisItem);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            db.Entry(product).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            var thisItem = db.Products.FirstOrDefault(products => products.ProductId == id);
+            return View(thisItem);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisItem = db.Products.FirstOrDefault(products => products.ProductId == id);
+            db.Products.Remove(thisItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
 
